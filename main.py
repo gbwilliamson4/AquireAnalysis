67f31aeba7f934ca3a84ade884cd38773a7dead4
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2021-12-26 20:29:23 -0700

    Main test works. Now time to make parent classes for each company with child classes being the individual ones. Then figure out a good way to show totals. Bar graphs? Tables?

--- main.py
+++ main.py
@@ -3,46 +3,61 @@ from Players import Players
 from American import American
 
 st.title("George's Aquire Test")
-player_list = []
-P1 = Players()
 
+# Setup player objects
+player_list = []
+P1 = Players("P1")
+P2 = Players("P2")
+P3 = Players("P3")
+P4 = Players("P4")
+
+player_list.append(P1)
+player_list.append(P2)
+player_list.append(P3)
+player_list.append(P4)
 
 American = American()
 
-# Create and add players to list.
-for i in range(int(st.slider("Players", 2, 5))):
-    player_list.append(Players(st.text_input(f'Player {i}')))
-
-# Assign key values to each player to avoid errors when forms are loading.
-for i in range(len(player_list)):
-    player_list[i].key = i
-
-for player in player_list:
-    player.holdings = st.number_input(f"American holdings for {player.player_name}.", 0, 100, key=player.key)
-    print(player.holdings)
-
+# Setup display of web
+# Display number of company squares at the very top, then have everything else split into columns
 American.squares = st.number_input("Current American Squares", 0, 100)
+American.value_update()
+
+col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
+with col1:
+    P1.player_name = st.text_input(P1.player_name)
+    P1.american_holdings = st.number_input(f"American Shares for {P1.player_name}", 0, 10)
+    st.write(f"{P1.player_name} currently has ${P1.american_holdings * American.vps} worth of American")
+
+
+with col2:
+    P2.player_name = st.text_input(P2.player_name)
+    P2.american_holdings = st.number_input(f"American Shares for {P2.player_name}", 0, 10)
+    st.write(f"{P2.player_name} currently has ${P2.american_holdings * American.vps} worth of American")
+
+
+with col3:
+    P3.player_name = st.text_input(P3.player_name)
+    P3.american_holdings = st.number_input(f"American Shares for {P3.player_name}", 0, 10)
+    st.write(f"{P3.player_name} currently has ${P3.american_holdings * American.vps} worth of American")
+
+
+with col4:
+    P4.player_name = st.text_input(P4.player_name)
+    P4.american_holdings = st.number_input(f"American Shares for {P4.player_name}", 0, 10)
+    st.write(f"{P4.player_name} currently has ${P4.american_holdings * American.vps} worth of American")
+
+
+
 
 if st.button('stuff'):
     st.write("The button has been pressed")
     for player in player_list:
         American.value_update()
         # print(player.player_name)
-        st.write(f"{player.player_name} currently has {player.holdings} shares of American.")
+        st.write(f"{player.player_name} currently has {player.american_holdings} shares of American.")
         st.write(f"There are currently {American.squares} out on the market.")
         st.write(f"American is currently valued at {American.vps}")
-        st.write(f"{player.player_name} currently has ${American.vps * player.holdings} worth of American.")
+        st.write(f"{player.player_name} currently has ${American.vps * player.american_holdings} worth of American.")
 
-col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
 
-with col1:
-    st.number_input("Col1", 0, 10)
-
-with col2:
-    st.number_input("Col2", 0, 10)
-
-with col3:
-    st.number_input("Col3", 0, 10)
-
-with col4:
-    st.number_input("Col4", 0, 10)
