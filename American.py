1039c92aec173011983dc532d29a05f532b8ffe7
gbwilliamson4 <90010953+gbwilliamson4@users.noreply.github.com>
2021-12-27 19:04:59 -0700

    Parent classes for Tier 2 companies has been created. Next we will format the web page to be a bit more clean

--- American.py
+++ American.py
@@ -1,26 +1,41 @@
-class American:
-    def __init__(self):
-        self.vps = 0
-        self.squares = 0
-
-    def value_update(self):
-        if self.squares == 0:
-            self.vps = 0
-        elif self.squares == 2:
-            self.vps = 300
-        elif self.squares == 3:
-            self.vps = 400
-        elif self.squares == 4:
-            self.vps = 500
-        elif self.squares == 5:
-            self.vps = 600
-        elif 6 <= self.squares <= 10:
-            self.vps = 700
-        elif 11 <= self.squares <= 20:
-            self.vps = 800
-        elif 21 <= self.squares <= 30:
-            self.vps = 900
-        elif 31 <= self.squares <= 40:
-            self.vps = 1000
-        elif self.squares >= 41:
-            self.vps = 1100
+# class Tier2:
+#     def __init__(self):
+#         self.vps = 0
+#         self.squares = 0
+#
+#     def value_update(self):
+#         if self.squares == 0:
+#             self.vps = 0
+#         elif self.squares == 2:
+#             self.vps = 300
+#         elif self.squares == 3:
+#             self.vps = 400
+#         elif self.squares == 4:
+#             self.vps = 500
+#         elif self.squares == 5:
+#             self.vps = 600
+#         elif 6 <= self.squares <= 10:
+#             self.vps = 700
+#         elif 11 <= self.squares <= 20:
+#             self.vps = 800
+#         elif 21 <= self.squares <= 30:
+#             self.vps = 900
+#         elif 31 <= self.squares <= 40:
+#             self.vps = 1000
+#         elif self.squares >= 41:
+#             self.vps = 1100
+#
+#
+# class American(Tier2):
+#     def __init__(self):
+#         super().__init__()
+#
+#
+# class Worldwide(Tier2):
+#     def __init__(self):
+#         super().__init__()
+#
+#
+# class Festival(Tier2):
+#     def __init__(self):
+#         super().__init__()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from Players import Players
-from American import American
+from Tier2 import American, Worldwide, Festival
 
 st.title("George's Aquire Test")
 
@@ -17,47 +17,68 @@ player_list.append(P3)
 player_list.append(P4)
 
 American = American()
+Worldwide = Worldwide()
+Festival = Festival()
 
 # Setup display of web
 # Display number of company squares at the very top, then have everything else split into columns
 American.squares = st.number_input("Current American Squares", 0, 100)
-American.value_update()
+Worldwide.squares = st.number_input("Current Worldwide Squares", 0, 100)
+Festival.squares = st.number_input("Current Festival Squares", 0, 100)
+
+
+def update_companies():
+    American.value_update()
+    Worldwide.value_update()
+    Festival.value_update()
+
+
+update_companies()
 
 col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
+
 with col1:
     P1.player_name = st.text_input(P1.player_name)
     P1.american_holdings = st.number_input(f"American Shares for {P1.player_name}", 0, 10)
     st.write(f"{P1.player_name} currently has ${P1.american_holdings * American.vps} worth of American")
-
 
 with col2:
     P2.player_name = st.text_input(P2.player_name)
     P2.american_holdings = st.number_input(f"American Shares for {P2.player_name}", 0, 10)
     st.write(f"{P2.player_name} currently has ${P2.american_holdings * American.vps} worth of American")
 
-
 with col3:
     P3.player_name = st.text_input(P3.player_name)
     P3.american_holdings = st.number_input(f"American Shares for {P3.player_name}", 0, 10)
     st.write(f"{P3.player_name} currently has ${P3.american_holdings * American.vps} worth of American")
-
 
 with col4:
     P4.player_name = st.text_input(P4.player_name)
     P4.american_holdings = st.number_input(f"American Shares for {P4.player_name}", 0, 10)
     st.write(f"{P4.player_name} currently has ${P4.american_holdings * American.vps} worth of American")
 
+st.header("Analysis")
 
+col1, col2, col3, col4 = st.columns((1, 1, 1, 1))
+with col1:
+    st.write("this is the first column")
+
+with col2:
+    st.write("this is the second column")
+
+with col3:
+    st.write("this is the third column")
+
+with col4:
+    st.write("this is the fourth column")
 
 
 if st.button('stuff'):
     st.write("The button has been pressed")
     for player in player_list:
-        American.value_update()
+        update_companies()
         # print(player.player_name)
         st.write(f"{player.player_name} currently has {player.american_holdings} shares of American.")
         st.write(f"There are currently {American.squares} out on the market.")
         st.write(f"American is currently valued at {American.vps}")
         st.write(f"{player.player_name} currently has ${American.vps * player.american_holdings} worth of American.")
-
-
